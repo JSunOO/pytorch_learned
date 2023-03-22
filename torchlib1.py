6b68903939dbed2ed97d5cae57485c074d383791
JSunOO <swoouimn@gmail.com>
2023-03-22 10:41:38 +0900

    baseline_1.py를 위한 공통함수 적용 테스트

---
+++ torchlib1.py
@@ -0,0 +1,8 @@
+import torch
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
